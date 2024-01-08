091eabb334660819dd5ed74f43a4c121a600188d
Ali Rostami <151998024+AliRostami2023@users.noreply.github.com>
2024-01-08 09:43:52 +0330

    Update settings.py

--- celestial_art/settings.py
+++ celestial_art/settings.py
@@ -135,8 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'alirostami1253@gmail.com'
-EMAIL_HOST_PASSWORD = 'rxcorfqiarllawbu'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
